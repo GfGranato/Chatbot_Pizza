76c7866ec32f95661cdbdc7f492bc5d7e37b1c82
Guilherme Granato <42078512+GfGranato@users.noreply.github.com>
2019-04-24 10:32:22 -0300

    Update BotPizza_tentativa.py

--- BotPizza_tentativa.py
+++ BotPizza_tentativa.py
@@ -13,6 +13,7 @@ bot = telepot.Bot(token='830025759:AAGFXGtpWrWyEkWI_eQO1Q6karejEZv8Yxs')
 
 #tentativa com o incicio,  chat e callback dentro de messageLoop tudo junto
 
+nivel = 0
 nome =''
 endereco =''
 pizza = ''
@@ -20,13 +21,13 @@ tamanhoP =''
 refrigerante = ''
 tamanhoR = ''
     #Comeco do programa
-@bot.message_handler(content_type == 'text')
+
 def inicio(msg):
     global nome, endereco
         
     global pizza, tamanhoP
     global refrigerante, tamanhoR
-        
+    global nivel
 
     content_type, chat_type, chat_id = telepot.glance(msg)
 
@@ -34,28 +35,36 @@ def inicio(msg):
     cardapio_pizza= {1:'1 pizza calabreza', 2:'2 pizza 4 queijos', 3: '3 pizza escarola'}
     cardapio_refri= {1:'1 Coca-cola', 2:'2 Fanta-uva', 3:'3 Guaraná'}
 
-    bot.sendMessage(chat_id, 'Olá, qual é o seu nome?')
-    nome = msg['text']
-    endereco = bot.send_location(chat_id, lat, lon)
+    if nivel == 0:
+        if content_type == 'text': 
+            bot.sendMessage(chat_id, 'Olá, qual é o seu nome?')
+            nome = msg['text']
+            nivel+=1
 
 #cardapio e pedido de pizza.
-    bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para pizza')
-    for i in cardapio_pizza:
-        bot.sendMessage(chat_id, cardapio_pizza[i])  
-    pizza = cardapio_pizza[int(msg['text'])]
+    if nivel ==1:
+        if content_type =='text':
+            bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para pizza')
+            for i in cardapio_pizza:
+                bot.sendMessage(chat_id, cardapio_pizza[i])  
+            pizza = cardapio_pizza[int(msg['text'])]
+            nivel+=1
+            
+   ''' arrumar os niveis a partir daqui'''
 
 ###tentativa de tamanho da pizza com bot'oes###
     keyboard = ReplyKeyboardMarkup( keyboard= [[KeyboardButton(text ='Brotinho'),KeyboardButton(text ='Média'),KeyboardButton(text ='Grande')]],resize_keyboard = True)
-
+    tamanhoP = tamanhos(chatID, keyboard)
+    
 #cardapio e pedido de refrigerante.
     bot.sendMessage(chat_id, 'Agora seu refrigerante ')
     for refrigerante in cardapio_refri:
         bot.sendMessage(chat_id, cardapio_refri[refrigerante])
     refrigerante = cardapio_refri[int(msg['text'])]
-
+    
 ###tentativa de tamanho do refrigerante com bot'oes###
     keyboard = ReplyKeyboardMarkup( keyboard= [[KeyboardButton(text ='600 mL'),KeyboardButton(text ='1.5L'), KeyboardButton(text ='2L'), KeyboardButton(text ='Não quero bebidas')]],resize_keyboard = True)
-
+    tamanhoR = tamanhos(chatID, keyboard)
 ''' falta colocar as opcoes para caso o usuario
     queira pedir mais pizzas e refrigerantes.
     para isso eh necessario adicionar os bot'oes sim e nao,
@@ -67,8 +76,9 @@ def inicio(msg):
             
     bot.sendMessage(chat_id, refrigerante[1:] + 'de '+ tamanhoR)
            
-
-    bot.sendMessage(chat_id, "No endereço: {}".format(endereco))
+    ''' ARRUMAR  A FUNÇÃO DO ENDEREÇO '''
+    
+    bot.sendMessage(chat_id, "No endereço: {}".format(bot.send_location(chat_id, lat, lon)))
     bot.sendMessage(chat_id, 'Seu pedido chegara em instantes.')
 
 
@@ -77,23 +87,30 @@ def tamanhos(chat_id, msg):
 ###tamanhos pizza
     if (msg == 'Brotinho'):
         tamanhoP = 'Brothinho'
+        return tamanhoP
     elif (msg == 'Média'):
         tamanhoP = 'Média'
+        return tamanhoP
     elif (msg == 'Grande'):
         tamanhoP = 'Grande'
+        return tamanhoP
 ###tamanhos REFRIGERANTE
     elif (msg == '600 mL'):
         tamanhoR = '600 mL'
+        return tamanhoR
     elif (msg == '1.5L'):
         tamanhoR = '1.5L'
+        return tamanhoR
     elif(msg == '2L'):
         tamanhoR = '2L'
+        return tamanhoR
 
     elif (msg == 'Não quero bebidas'):
         tamanhoR = ''
+        return tamanhoR
     
 
-MessageLoop(bot, inicio,{'chat': on_chat_message, 'callback_query': on_callback_query}).run_as_thread()
+MessageLoop(bot, inicio).run_as_thread()
 
 print ('Listening ...')
 
