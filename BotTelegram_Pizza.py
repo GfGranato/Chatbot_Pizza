52e999e0534d8310a9b7bddddd185bf0218dc861
Guilherme Granato <42078512+GfGranato@users.noreply.github.com>
2019-05-30 17:06:21 -0300

    Update BotTelegram_Pizza.py

--- BotTelegram_Pizza.py
+++ BotTelegram_Pizza.py
@@ -5,9 +5,6 @@ import telepot
 import telegram
 
 from unidecode import unidecode
-#opção para botão: types
-#from telebot import types
-
 from telepot.loop import MessageLoop
 
 from telepot.namedtuple import  ReplyKeyboardMarkup, KeyboardButton
@@ -32,16 +29,15 @@ def handle(msg):
     global nivel
     global valor
     global valorpedido
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    #print(content_type, chat_type, chat_id)
     
-          
+    content_type, chat_type, chat_id = telepot.glance(msg)
+  
     #Cardápios
     cardapio_pizza= {1:'1 Pizza calabreza', 2:'2 Pizza 4 queijos', 3: '3 Pizza escarola'}
-    valor_pizza= {1: 20.00, 2: 22.00, 3: 19.00}
+    valor_pizza= {1: 25.99, 2: 22.80, 3: 27.15}
     valor_PTamanho= {"Brotinho": 0.8, "Média": 1, "Grande": 1.2}
     cardapio_refri= {1:'1 Coca-cola', 2:'2 Fanta-uva', 3:'3 Guaraná'}
-    valor_refri= {1: 6.25, 2: 5.00, 3: 5.00}
+    valor_refri= {1: 4.2, 2: 3.81, 3: 3.27}
     valor_RTamanho= {"600 mL": 0.8, "1.5 L": 1, "2 L": 1.2}    
     #usuário cadastro
     if nivel == 0:
@@ -63,13 +59,11 @@ def handle(msg):
         nivel+=1
     elif nivel ==3:
         complemento = (msg['text'])
-        bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para pizza')
 
         media = r"https://github.com/GfGranato/Chatbot_Pizza/blob/master/pizzaboy.PNG?raw=true"
-        bot.sendPhoto(chat_id, media, caption='Cardápio')
+        bot.sendPhoto(chat_id, media, caption='Este é o nosso Cardápio')
 
-        #for piz in cardapio_pizza:
-         #   bot.sendMessage(chat_id, cardapio_pizza[piz])                
+        bot.sendMessage(chat_id, 'Pode fazer seu pedido, digite o numero para a opção de pizza')            
                 
         nivel+=1
            
@@ -95,7 +89,7 @@ def handle(msg):
         if content_type == 'text':
             
             tamanhoP.append(msg['text'])
-            ##
+        
             valor += valorpedido*(valor_PTamanho[(msg['text'])])
             valorpedido=0
 
@@ -111,18 +105,13 @@ def handle(msg):
         
         if (msg['text'].lower() == 'sim'):
            
-            bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para pizza')
+            bot.sendMessage(chat_id, 'Ok,  digite o numero para a próxima de pizza')
             nivel = 4
         elif (unidecode(msg['text'].lower()) == 'nao'):
 
-            bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para refrigerante')
-            
-            for ref in cardapio_refri:
-                bot.sendMessage(chat_id, cardapio_refri[ref])                
-                
+            bot.sendMessage(chat_id, 'Ok, digite o número para a opção de refrigerante')              
             nivel = 7
 
-    
     elif nivel == 7:
         if content_type == 'text':            
             refrigerante.append(cardapio_refri[int(msg['text'])])
@@ -134,7 +123,7 @@ def handle(msg):
                     KeyboardButton(text="2 L"),
                                 
                         ]],resize_keyboard=True)
-            bot.sendMessage(chat_id, 'qual o tamanho do refrigerante?', reply_markup=keyboard )
+            bot.sendMessage(chat_id, 'Qual o tamanho do refrigerante?', reply_markup=keyboard )
             
         nivel+=1
     
@@ -155,18 +144,16 @@ def handle(msg):
             
 
         nivel+=1
-        
-    ### arrumar nivel 9 sim e nao
-    
+
     elif nivel == 9:
         if content_type == 'text':
             if(msg['text'].lower() == 'sim'):
            
-                bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para refrigerante')
+                bot.sendMessage(chat_id, 'Ok, digite o numero para o próximo refrigerante')
                 nivel = 7
             elif (unidecode(msg['text'].lower()) == 'nao'):
               
-                bot.sendMessage(chat_id, 'Obrigado '+ nome +', seu pedido será entregue no endereço '+endereco+' '+complemento)
+                bot.sendMessage(chat_id, 'Obrigado '+ nome +', seu pedido será entregue no endereço: '+endereco+', '+complemento)
                 bot.sendMessage(chat_id, 'Seu pedido é: ')
                 for k in range(len(pizza)):
                     bot.sendMessage(chat_id, pizza[k][2:] + ' de tamanho ' + tamanhoP[k])
@@ -189,6 +176,7 @@ def handle(msg):
             if (msg['text'].lower() == 'sim'):
                 
                 bot.sendMessage(chat_id, 'Obrigado pela preferência!')
+                nivel +=1
             elif (unidecode(msg['text'].lower()) == 'nao'):
                 
                 pizza = []
@@ -199,8 +187,7 @@ def handle(msg):
                 valorpedido = 0
                 nivel = 4
                 bot.sendMessage(chat_id, 'Faça seu pedido, digite o numero para a opção para pizza')
-
-
+                
 bot = telepot.Bot(token='830025759:AAGFXGtpWrWyEkWI_eQO1Q6karejEZv8Yxs')
 MessageLoop(bot, handle).run_as_thread()
 print ('Listening ...')
